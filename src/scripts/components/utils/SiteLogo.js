f4140e8b060f011d9272f175555d47f8d44bf36b
Feller <tfellert@gmail.com>
2019-04-16 19:13:19 +0300

    Commit 16/4

--- src/scripts/components/utils/SiteLogo.js
+++ src/scripts/components/utils/SiteLogo.js
@@ -7,7 +7,7 @@ class SiteLogo extends React.Component {
         return (
             <Image width={width ? width : '100px'}
                    src={'http://oleo-admin.tf-interactive.com/wp-content/uploads/2019/04/oleo_logsimbol_big.png'}
-                   style={{display: 'block', margin: 'auto'}}/>
+                   style={{display: ' block', margin: 'auto'}}/>
         )
     }
 }
